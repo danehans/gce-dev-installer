370214e7e08ab5e35033962d924d02307261c663
Daneyon Hansen <danehans@cisco.com>
2018-08-16 12:43:46 -0700

    Initial functional version of tool.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# gce-dev-installer
-Simple install script to create a compute instances for development purposes on Google Cloud Platform.
+# gdev-installer
+gdev-installer is a tool for creating a compute instance for development purposes on Google Cloud Platform.
+
+## Prerequisites
+
+[gcloud cli tool](https://cloud.google.com/sdk/docs/)
+
+## Installation
+
+gdev-installer requires one command:
+```bash
+curl -L https://git.io/gdev-installer  | sh -
+```
+
+## Customizing the Installation
+
+Environment variables can be used to customize the installation. Review the
+[installer](https://github.com/danehans/gdev-installer/blob/master/install.sh) to learn more about the
+supported options. Setting options take the form of:
+```bash
+export OPTION=VALUE
+```
+
+For example, to change the name of the GCP instance (defaults to "dev"):
+```bash
+export VM_NAME=my_gcp_instance
+```
+
+Then run the installer:
+```bash
+curl -L https://git.io/gdev-installer  | sh -
+```
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+PROJECT=${PROJECT:-$(gcloud config list --format 'value(core.project)' 2>/dev/null)}
+ZONE=${ZONE:-$(gcloud config list --format 'value(compute.zone)' 2>/dev/null)}
+MACHINE_TYPE=${MACHINE_TYPE:-n1-highcpu-2}
+VM_NAME=${VM_NAME:-dev}
+
+# Check for gcloud cli tool
+gcloud version > /dev/null
+exit_status=$?
+if [ $exit_status -eq 1 ]; then
+    echo "### This script requires the gcloud cli tool"
+    echo "### Details: https://cloud.google.com/sdk/docs/"
+    exit 1
+fi
+
+# Get the latest Ubuntu image.
+echo "### Obtaining the latest Ubuntu Xenial image name..."
+VM_IMAGE=$(gcloud compute images list --standard-images --filter=name~ubuntu-1604-xenial --limit=1 --uri)
+
+# Create the gcloud compute instance.
+echo "### Creating VM_NAME=$VM_NAME using VM_IMAGE=$VM_IMAGE"
+gcloud compute instances create "$VM_NAME" --zone "$ZONE" --machine-type "$MACHINE_TYPE" \
+--image "$VM_IMAGE" --project "$PROJECT"
+sleep 5
+
+echo "### Install complete. VM $VN_NAME status:"
+gcloud compute instances list --filter="name=( '$VM_NAME' ... )"
